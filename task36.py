3ebc4ec1a06f559ef1eaed90d53aab1f57f1dd00
killerror <94983135+killerror@users.noreply.github.com>
2023-03-12 22:38:52 +0400

    Update task36.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- task36.py
+++ task36.py
@@ -2,7 +2,7 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows+1):
         row = []
         for j in range(1, num_columns+1):
-            row.append(str(operation(i, j)))
+            row.append(str(operation(i, j)).rjust(len(str(operation(num_rows, j)))))
         print(" ".join(row))
 
 print_operation_table(lambda x, y: x * y)
