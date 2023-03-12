2fe990ddd0c48f40d8eadbbe1c62a9ee07c78515
reeNUT <rmurzaliev@yandex.ru>
2023-03-12 20:50:03 +0400

    homework7

--- task38.py
+++ task38.py
@@ -1,24 +1,20 @@
 def count_vowels(word):
-    """Функция для подсчета гласных букв в слове"""
-    vowels = 'аеёиоуыэюя'  # список гласных букв
+    vowels = 'аеёиоуыэюя'
     count = 0
     for letter in word:
         if letter.lower() in vowels:
             count += 1
     return count
 
-
 def check_rhythm(poem):
-    """Функция для проверки ритма стихотворения"""
-    words = poem.split()  # разбиваем стихотворение на слова
-    syllables = []  # список количества слогов в каждой фразе
+    words = poem.split()
+    syllables = []
     for word in words:
         syllables.append(count_vowels(word))
     if len(set(syllables)) == 1:
         return "Парам пам-пам"
     else:
         return "Пам парам"
-
 
 poem = input("Введите стихотворение: ")
 print(check_rhythm(poem))
